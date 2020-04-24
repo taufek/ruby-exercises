e2b572c7ffbe07f6604a98f049e35ae9d6d3782b
Taufek Johar <taufek@gmail.com>
2020-04-25 01:14:12 +0800

    Install rspec

---
+++ lib/sorting/bubble_sort.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class BubbleSort
+  def self.sort
+
+  end
+end
---
+++ spec/sorting/bubble_sort_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'lib/sorting/bubble_sort'
+
+RSpec.describe BubbleSort do
+  describe '.sort' do
+    subject { described_class.sort }
+  end
+end
