8a5c97f4ae6409ae77cb9c90c6a840aeae740986
Taufek Johar <taufek@gmail.com>
2020-04-25 01:33:04 +0800

    Bubble Sort

--- lib/sorting/bubble_sort.rb
+++ lib/sorting/bubble_sort.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 module Sorting
+  # https://en.wikipedia.org/wiki/Bubble_sort
   class BubbleSort
-    def self.sort
+    def self.sort(list)
+      swap = true
+      while swap
+        swap = false
 
+        (list.length - 1).times do |i|
+          if list[i] > list[i + 1]
+            list[i], list[i + 1] = list[i + 1], list[i]
+            swap = true
+          end
+        end
+      end
+      list
     end
   end
 end
--- spec/sorting/bubble_sort_spec.rb
+++ spec/sorting/bubble_sort_spec.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe Sorting::BubbleSort do
+
   describe '.sort' do
-    subject { described_class.sort }
+    subject { described_class.sort(input) }
+
+    context 'multiple numbers' do
+      let(:input) { [10, 6, 4, 9, 1] }
+
+      it { is_expected.to eq input.sort }
+    end
+
+    context 'single number' do
+      let(:input) { [10] }
+
+      it { is_expected.to eq input }
+    end
   end
 end
